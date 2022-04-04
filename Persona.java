03e877e063f7f5cd44f953fa56c4fe6fed8187fe
raul <raulcamacho153@gmail.com>
2022-04-04 11:41:22 +0200

    añadir poblacion

--- Persona.java
+++ Persona.java
@@ -5,12 +5,14 @@ public class Persona {
     String nombre;
     String apellidos;
     int edad;
+    private String poblacion;
 
-    public Persona(String dni, String nombre, String apellidos, int edad) {
+    public Persona(String dni, String nombre, String apellidos, int edad, String poblacion) {
         this.dni = dni;
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
+        this.poblacion = poblacion;
     }
 
 }
